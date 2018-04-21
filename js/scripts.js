5654d11b3d87429c5048ae9a3f6c8e87a9d42227
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-04-22 00:11:06 +0200

    correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 $(function() {
-  // generujemy unikalne ID aby nie bylo duplikatow
-
   function randomString() {
     var chars = "0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ";
     var str = "";
@@ -10,16 +8,12 @@ $(function() {
     return str;
   }
 
-  // tworzymy funkcje konstruujaca klase Column
-
   function Column(name) {
     var self = this;
 
     this.id = randomString();
     this.name = name;
     this.$element = createColumn();
-
-    // tworzymy kolumne
 
     function createColumn() {
       var $column = $("<div>").addClass("column");
@@ -34,24 +28,13 @@ $(function() {
         .addClass("add-card fa fa-plus-square")
         .text("");
 
-      // kasowanie kolumny po kliknieciu w przycisk
-
-      $columnDelete.click(function() {
+      $columnDelete.on("click", function() {
         self.removeColumn();
       });
 
-      // dodaj notatke po kliknieciu w przycisk
-
-      $columnAddCard.click(function() {
-        var card = prompt("Enter the name of the card");
-        if (card) {
-          self.addCard(new Card(card));
-        } else if (card.length === 0) {
-          self.addCard(new Card(card));
-        }
+      $columnAddCard.on("click", function() {
+        self.addCard(new Card(prompt("Enter the name of the card")));
       });
-
-      // konstruowanie kolumny
 
       $column
         .append($columnTitle)
@@ -63,8 +46,6 @@ $(function() {
     }
   }
 
-  // dodanie prototypu
-
   Column.prototype = {
     addCard: function(card) {
       this.$element.children("ul").append(card.$element);
@@ -74,16 +55,12 @@ $(function() {
     }
   };
 
-  // tworzymy funkcje konstruujaca klase Card
-
   function Card(description) {
     var self = this;
 
     this.id = randomString();
     this.description = description;
     this.$element = createCard();
-
-    // tworzymy karte
 
     function createCard() {
       var $card = $("<li>").addClass("card");
@@ -94,13 +71,9 @@ $(function() {
         .addClass("btn-delete")
         .text("x");
 
-      // kasowanie karty po kliknieciu w przycisk(wewnatrz metody createCard())
-
-      $cardDelete.click(function() {
+      $cardDelete.on(function() {
         self.removeCard();
       });
-
-      // konstruujemy karte(wewnatrz metody createCard())
 
       $card.append($cardDelete).append($cardDescription);
 
@@ -108,15 +81,11 @@ $(function() {
     }
   }
 
-  // dodanie prototypu
-
   Card.prototype = {
     removeCard: function() {
       this.$element.remove();
     }
   };
-
-  // tworzymy obiekt tablicy i przypinamy nasluchiwanie zdarzen
 
   var board = {
     name: "Kanban Board",
@@ -127,8 +96,6 @@ $(function() {
     $element: $("#board .column-container")
   };
 
-  // implementacja funkcji initSortable(drag and drop)
-
   function initSortable() {
     $(".column-card-list")
       .sortable({
@@ -138,9 +105,7 @@ $(function() {
       .disableSelection();
   }
 
-  // podpiecie do przycisku tablicy wrzucanie nowej kolumny do tablicy
-
-  $(".create-column").click(function() {
+  $(".create-column").on("click", function() {
     var name = prompt("Enter a column name");
 
     if (name) {
@@ -151,26 +116,16 @@ $(function() {
     }
   });
 
-  // tworzenie elementow w kanbanie
-
-  // kolumny
-
   var todoColumn = new Column("To do");
   var doingColumn = new Column("Doing");
   var doneColumn = new Column("Done");
-
-  // dodanie kolumn do tablicy
 
   board.addColumn(todoColumn);
   board.addColumn(doingColumn);
   board.addColumn(doneColumn);
 
-  // tworzenie kart
-
   var card1 = new Card("New task");
   var card2 = new Card("Create kanban boards");
-
-  // dodanie kart do kolumn
 
   todoColumn.addCard(card1);
   doingColumn.addCard(card2);
